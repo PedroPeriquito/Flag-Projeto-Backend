2a65ada813e99660e5d5d5e8cad53fc1c052332d
PedroPeriquito <pedro_perikito@hotmail.com>
2024-04-26 18:07:21 +0100

    review fixed

--- Backend/src/routes/userRouter.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const userController = require('../controllers/userController');
-
-router.post('/', userController.postUser);
-
-
-module.exports = router;
